6fc287f49bed6abbcc8c549ab12ddce39917a0f3
Kurt Schwehr <schwehr@google.com>
2021-01-31 00:35:48 -0800

    dbfinfo.c: Cleanup
    - Remove unused variables
    - Localize variables
    - Add scope to allow const on i
    - Combine definition and initialization

--- contrib/dbfinfo.c
+++ contrib/dbfinfo.c
@@ -4,41 +4,20 @@
  * This code is in the public domain.
  *
  * This code is based in part on the earlier work of Frank Warmerdam
- *
- * requires shapelib 1.2
- *   gcc dbfinfo dbfopen.o dbfinfo
- *
  */
 
 #include "shapefil.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv)
-
-{
-  DBFHandle hDBF;
-  int *panWidth, i, iRecord;
-  char szFormat[32], szField[1024];
-  char ftype[32], cTitle[32], nTitle[32];
-  int nWidth, nDecimals;
-  int cnWidth, cnDecimals;
-  DBFHandle cDBF;
-  DBFFieldType hType, cType;
-  int ci, ciRecord;
-
-  /* -------------------------------------------------------------------- */
-  /*      Display a usage message.                                        */
-  /* -------------------------------------------------------------------- */
+int main(int argc, char **argv) {
   if (argc != 2) {
     printf("dbfinfo xbase_file\n");
     exit(1);
   }
 
-  /* -------------------------------------------------------------------- */
-  /*      Open the file.                                                  */
-  /* -------------------------------------------------------------------- */
-  hDBF = DBFOpen(argv[1], "rb");
+  DBFHandle hDBF = DBFOpen(argv[1], "rb");
   if (hDBF == NULL) {
     printf("DBFOpen(%s,\"r\") failed.\n", argv[1]);
     exit(2);
@@ -49,19 +28,24 @@ int main(int argc, char **argv)
   /* -------------------------------------------------------------------- */
   /*	If there is no data in this file let the user know.		*/
   /* -------------------------------------------------------------------- */
-  i = DBFGetFieldCount(hDBF);
-  printf("%d Columns,  %d Records in file\n", i, DBFGetRecordCount(hDBF));
+  {
+    const int i = DBFGetFieldCount(hDBF);
+    printf("%d Columns,  %d Records in file\n", i, DBFGetRecordCount(hDBF));
+  }
 
   /* -------------------------------------------------------------------- */
   /*	Compute offsets to use when printing each of the field 		*/
   /*	values. We make each field as wide as the field title+1, or 	*/
   /*	the field value + 1. 						*/
   /* -------------------------------------------------------------------- */
-  panWidth = (int *)malloc(DBFGetFieldCount(hDBF) * sizeof(int));
+  int *panWidth = (int *)malloc(DBFGetFieldCount(hDBF) * sizeof(int));
 
-  for (i = 0; i < DBFGetFieldCount(hDBF); i++) {
+  char ftype[32];
+  int nDecimals;
+  int nWidth;
+
+  for (int i = 0; i < DBFGetFieldCount(hDBF); i++) {
     char szTitle[12];
-    DBFFieldType eType;
 
     switch (DBFGetFieldInfo(hDBF, i, szTitle, &nWidth, &nDecimals)) {
     case FTString:
